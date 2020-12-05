6681dd273b2c4e4f2ed789ad47af0ea069feb3a1
aba450 <54678566+aba450@users.noreply.github.com>
2020-12-04 20:13:58 -0500

    added some more utitlities

--- datasets/preparation/util.py
+++ datasets/preparation/util.py
@@ -1,8 +1,6 @@
 import math
 import requests
-# from tqdm import tqdm
-
-# from .misc import sizeof_fmt
+from tqdm import tqdm
 
 
 def download_file_from_google_drive(file_id, save_path):
@@ -34,3 +32,47 @@ def download_file_from_google_drive(file_id, save_path):
         file_size = None
 
     save_response_content(response, save_path, file_size)
+
+    def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+
+def sizeof_fmt(size, suffix='B'):
+    """Get human readable file size.
+    Args:
+        size (int): File size.
+        suffix (str): Suffix. Default: 'B'.
+    Return:
+        str: Formated file siz.
+    """
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(size) < 1024.0:
+            return f'{size:3.1f} {unit}{suffix}'
+        size /= 1024.0
+    return f'{size:3.1f} Y{suffix}'
+def save_response_content(response,
+                          destination,
+                          file_size=None,
+                          chunk_size=32768):
+    if file_size is not None:
+        pbar = tqdm(total=math.ceil(file_size / chunk_size), unit='chunk')
+
+        readable_file_size = sizeof_fmt(file_size)
+    else:
+        pbar = None
+
+    with open(destination, 'wb') as f:
+        downloaded_size = 0
+        for chunk in response.iter_content(chunk_size):
+            downloaded_size += chunk_size
+            if pbar is not None:
+                pbar.update(1)
+                pbar.set_description(f'Download {sizeof_fmt(downloaded_size)} '
+                                     f'/ {readable_file_size}')
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+        if pbar is not None:
+            pbar.close()
